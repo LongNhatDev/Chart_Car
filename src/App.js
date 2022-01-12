d5e4456903c95fc2091ec68f9706974f28e8f3ad
Tran Long Nhat <87260916+LongNhatDev@users.noreply.github.com>
2022-01-12 16:37:12 +0700

    check branch master

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p> Tran Long Nhat</p>
+        <p> Branch Master</p>
         <a
           className="App-link"
           href="https://reactjs.org"
