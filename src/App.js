c870d6fae178fe255ca7f90dedf47338229ca1fd
Long Nhat <tranlongnhat33333@gmail.com>
2022-01-20 16:52:33 +0700

    Add feature to show list item

--- src/App.js
+++ src/App.js
@@ -5,24 +5,27 @@ import React, { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expense from './components/Expense/Expense';
 
-
 const DUMMY_EXPENSES = [
   {
+    id:"1",
     title: "Cadilac Luxury",
     amount: 294.67,
     date: new Date(2021, 7, 18)
   },
   {
+    id:"2",
     title: "Vinfast Turbo 2.0",
     amount: 194.67,
     date: new Date(2022, 12, 3)
   },
   {
+    id:"3",
     title: "Lexus LX570",
     amount: 888.22,
     date: new Date(2022, 8, 25)
   },
   {
+    id:"4",
     title: "Mec MayBach",
     amount: 777.67,
     date: new Date(2021, 2, 11)
@@ -43,7 +46,7 @@ const App = () => {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expense item={expenses} />
+      <Expense item={expenses}/>
     </div>
   );
 }
--- src/components/Expense/ExpenseFilter.js
+++ src/components/Expense/ExpenseFilter.js
@@ -1,10 +1,9 @@
 import React from "react";
-import ExpenseChart from "./ExpenseChart";
 import "./ExpenseFilter.css"
 
 const ExpenseFilter = (props) => {
 
-    const dropdownChangeHandler=(event) =>{
+    const dropdownChangeHandler = (event) => {
         props.onChangeFilter(event.target.value);
     }
     return (
@@ -15,11 +14,10 @@ const ExpenseFilter = (props) => {
                     <option value="2021">2021</option>
                     <option value="2022">2022</option>
                     <option value="2023">2023</option>
+                    <option value="2024">2024</option>
+                    <option value="2025">2025</option>
                 </select>
             </div>
-            <ExpenseChart className="demoFilter"
-
-            />
         </div >
     )
 }
